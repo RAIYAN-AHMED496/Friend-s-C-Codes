32ad6b30e640547978f8c2e34f1f13521caf2b2c
RAIYAN-AHMED496 <meherabhossainraiyan@gmail.com>
2025-02-10 00:12:07 +0600

    1st code
    Sum of two numbers

---
+++ 01_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  // varibale declare;
+  int a = 12; // container
+  int b = 18;
+
+  int sum = a+b;
+
+
+  printf("SUM is = %d",sum); // integer ka dakanor jonno %d (format specifier);
+  return 0;
+}
