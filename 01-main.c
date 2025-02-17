1564925af1c194ee52025a0aa057d73b542060d6
RAIYAN-AHMED496 <meherabhossainraiyan@gmail.com>
2025-02-18 00:24:38 +0600

    Uploading sum c codes

---
+++ 01-main.c
@@ -0,0 +1,22 @@
+// Algoridthm
+// Step 1: Start
+// Step 2: num1 = 20,num2 = 25
+// Step 3: sum = num1+num2
+// Step 4: print sum
+// Step 5: End
+
+
+#include <stdio.h>
+
+int main(){ // int = integer = 1,2,3,4 // char = character = a,b,c
+  // sum 2 numbers 
+  int num1 = 20; // varibale (container)
+  int num2 = 25;
+
+
+  int sum = num1 + num2;
+
+  printf("The sum of these two numbers = %d",sum); // printf = "" // %d = integer format ki format tumi print korte chau %f
+  // printf("Sum = %d")
+  return 0;
+}
---
+++ 02-main.c
@@ -0,0 +1,23 @@
+// Algoridthm
+// Step 1: Start
+// Step 2: num1=20,num2=25,num3=30
+// Step3: sum = num1+num2+num3
+// Step 4: print sum
+// Step 5: End
+
+
+#include <stdio.h>
+
+int main(){ // int = integer = 1,2,3,4 // char = character = a,b,c
+  // sum 2 numbers 
+  int num1 = 20; // varibale (container)
+  int num2 = 25;
+  int num3 = 30;
+  // char a = 'A';
+
+  int sum = num1 + num2 + num3;
+
+  printf("Sum = %d",sum); // printf = "" // %d = integer format ki format tumi print korte chau %f
+  // printf("Sum = %d")
+  return 0;
+}
